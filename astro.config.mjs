4217ed5f2e16f268febcd2e4a30c55dfd5cf6891
Leonel Mujica <leonelmujica0203@gmail.com>
2026-09-12 15:47:04 +0200

    fix: tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://leodvlpr.github.io',
-  base: '/SEDT-porfolio/',
-  integrations: [tailwindcss()],
+  base: '/sedt-portfolio',
   vite: {
     plugins: [tailwindcss()]
   }
