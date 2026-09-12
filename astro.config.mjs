363d8669c8133d73030423894c57ed61a1fa12f5
Leonel Mujica <leonelmujica0203@gmail.com>
2026-09-12 15:34:48 +0200

    feat: portfolio ready for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://leodvlpr.github.io',
+  base: '/SEDT-porfolio/',
+  integrations: [tailwindcss()],
   vite: {
     plugins: [tailwindcss()]
   }
